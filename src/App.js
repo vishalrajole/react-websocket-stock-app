e9ef6991463115d01006b4965f107bbbfada7865
VishalRajole <vishal.rajole@mobikontech.com>
2018-11-28 11:58:38 +0530

    minor ws fix

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
   }
 
   componentDidMount() {
-    const socket = new window.WebSocket("wss://stocks.mnet.website");
+    const socket = new window.WebSocket("ws://stocks.mnet.website");
 
     socket.onopen = (event) => {
       socket.send('Ping!');
